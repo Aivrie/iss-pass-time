b4b57721805a13e64b283787ca1c98253a1f73c0
aivrie <ivoryokeke@gmail.com>
2021-03-14 21:14:08 +0100

    installed numpy

--- iss.py
+++ iss.py
@@ -1,9 +1,8 @@
+import numpy as np
 import requests
 import json
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent='geolocate.py')
-import numpy as np
-
 from datetime import datetime
 
 # Function to find the lat and long of inputted country
@@ -27,46 +26,47 @@ geocode = geolocate(location)
 latitude = geocode[0]
 longitude = geocode[1]
 
-print(geocode)
-print(latitude)
-print(longitude)
+# print(geocode)
+# print(latitude)
+# print(longitude)
 
 
-# parameters = {
-#     'lat': latitude,
-#     'lon': longitude
-# }
+parameters = {
+    'lat': latitude,
+    'lon': longitude
+}
 
-# # Requesting data from Open-notify API
-# request = requests.get('http://api.open-notify.org/iss-pass.json', params=parameters)
+# Requesting data from Open-notify API
+request = requests.get('http://api.open-notify.org/iss-pass.json', params=parameters)
 # print(request.status_code)
 
-# def jprint(obj):
-#     text = json.dumps(obj, sort_keys=True, indent=4)
-#     print(text)
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
     
-# response = request.json()
+response = request.json()
 # print(response)
 
-# # print(jprint(response))
+# print(jprint(response))
 
-# # rqst = response['request']
-# # # print(rqst)
+# rqst = response['request']
+# # print(rqst)
 
-# pass_time = response['response']
+pass_time = response['response']
 
-# risetime = []
-# for d in pass_time:
-#     time_stamp = d['risetime']
-#     risetime.append(time_stamp)
+risetime = []
+for d in pass_time:
+    time_stamp = d['risetime']
+    risetime.append(time_stamp)
 # print(risetime)
 
 # Converting to current time
-# times = []
+times = []
 
-# for rt in risetime:
-#     time = datetime.fromtimestamp(rt)
-#     print(time)
+for rt in risetime:
+    time = datetime.fromtimestamp(rt)
+    print(time)
+    
 
 
 
